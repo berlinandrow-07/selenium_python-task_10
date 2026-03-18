283f6db0b1f71d95e28d733f55b115314ca4c454
berlinandrow-07 <berlinandroy07@gmail.com>
2026-03-18 13:08:48 +0530

    ok

--- test_main.py
+++ test_main.py
@@ -13,24 +13,9 @@ def valid_login(user,passwords):
     password.send_keys(passwords)
     login=driver.find_element(By.ID,"login-button")
     login.click()
-    # assert "Swag Labs" in driver.title
-    # assert "https://www.saucedemo.com/" in page_url
-    # assert "inventory" in driver.current_url
     return driver
 
 
-# def test_invalid_login():
-#     page_url = "https://www.saucedemos.com/"
-#     driver.get(page_url)
-#     username=driver.find_element(By.ID,"user-name")
-#     username.send_keys("standard_users")
-#     password=driver.find_element(By.ID,"password")
-#     password.send_keys("secret_sauces")
-#     login=driver.find_element(By.ID,"login-button")
-#     login.click()
-#     assert "Swag labs" in driver.title
-#     assert "https://www.saucedemos.com/" in page_url
-#     assert "inventory" in driver.current_url
 
 def test_valid_title():
     driver=valid_login("standard_user","secret_sauce")
